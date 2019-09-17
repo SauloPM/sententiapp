b60d01e70f24a5d7cd345a4b84329e2ffa2eff1f
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-17 14:37:46 +0100

    [ General ] » Directorio 'js' añadido al directorio 'assets' para organizar mejor los ficheros JS

---
+++ src/assets/js/functions.js
@@ -0,0 +1,29 @@
+$(window).on("load", function() {
+
+    // ──────────────────── //
+    //     OWL CAROUSEL     //
+    // ──────────────────── //
+
+    // Configuration
+    // $('.loop').owlCarousel({
+    //     autoplay: false,
+    //     center: false,
+    //     dots: true,
+    //     items: 3,
+    //     loop: false,
+    //     mouseDrag: false,
+    //     nav: true,
+    //     navText: ["<",">"],
+    //     responsive : {
+    //         0 : {
+    //             items : 1,
+    //         },
+    //         550 : {
+    //             items : 2,
+    //         },
+    //         980 : {
+    //             items : 3,
+    //         }
+    //     }
+    // });
+});
