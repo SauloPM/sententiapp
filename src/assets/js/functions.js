96c305f30ba286ab0041956031ed34359abc1ada
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-17 15:06:14 +0100

    [ JS ] » Fichero vaciado

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,29 +1,3 @@
 $(window).on("load", function() {
 
-    // ──────────────────── //
-    //     OWL CAROUSEL     //
-    // ──────────────────── //
-
-    // Configuration
-    // $('.loop').owlCarousel({
-    //     autoplay: false,
-    //     center: false,
-    //     dots: true,
-    //     items: 3,
-    //     loop: false,
-    //     mouseDrag: false,
-    //     nav: true,
-    //     navText: ["<",">"],
-    //     responsive : {
-    //         0 : {
-    //             items : 1,
-    //         },
-    //         550 : {
-    //             items : 2,
-    //         },
-    //         980 : {
-    //             items : 3,
-    //         }
-    //     }
-    // });
 });
