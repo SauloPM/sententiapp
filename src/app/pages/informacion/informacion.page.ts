b7fae5e1ebc4e648f5da56325fda16535d2120ed
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-18 00:55:08 +0100

    [ Información ] » Estilos actualizados

--- src/app/pages/informacion/informacion.page.ts
+++ src/app/pages/informacion/informacion.page.ts
@@ -1,13 +1,17 @@
 import { Component      } from '@angular/core';
+import { NavController  } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+
+// Interfaces
+import { Fecha     } from '../../interfaces/fecha';
+import { Sentencia } from '../../interfaces/sentencia';
 
 // Servicios
 import { FechasService } from '../../services/fechas.service';
 
 @Component({
   selector: 'app-informacion',
-  templateUrl: './informacion.page.html',
-  styleUrls: ['./informacion.page.scss'],
+  templateUrl: './informacion.page.html'
 })
 export class InformacionPage {
 
@@ -17,19 +21,58 @@ export class InformacionPage {
     descripcion: '',
     imagen: ''
   };
+  
+  otrasFechas: Fecha    [] = [];
+  sentencias : Sentencia[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute, private servicioFechas: FechasService) {
+  // Configuración del carrusel
+  opcionesSlider = {
+    pagination: false,
+    spaceBetween: 5,
+    slidesPerView: 5,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    breakpoints: {
+      400: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      }
+    }
+  }
+
+  constructor(private activatedRoute: ActivatedRoute, private servicioFechas: FechasService, private navController: NavController) {
+
+    // Guardamos en una variable todas las fechas
+    this.servicioFechas.getFechas().subscribe( (data: Fecha[]) => {
+      this.otrasFechas = data;
+    });
+
+    // Obtenemos los parámetros de la URL (en este caso solo es el id de la fecha)
     this.activatedRoute.params.subscribe( parametroURL => {
+
+      // Guardamos en una variable los datos de la fecha cuyo ID se encuentra en la URL
       this.servicioFechas.getFecha( parametroURL.id ).subscribe( (data: Fecha[]) => {
         this.fecha = data[0];
       });
+
+      // Guardamos en una variable las sentencias de la fecha cuyo ID se encuentra en la URL
+      this.servicioFechas.getSentencias( parametroURL.id ).subscribe( ( data: Sentencia[] ) => {
+        this.sentencias = data;
+      });
     });
   }
-}
 
-export interface Fecha {
-  id: number;
-  etiqueta: string;
-  descripcion: string;
-  imagen: string;
+  // Volver a la página anterior
+  volverAtras() {
+    this.navController.back();
+  }
+
+  // Volver a la página de inicio
+  volverInicio() {
+    this.navController.navigateBack('/');
+  }
 }
