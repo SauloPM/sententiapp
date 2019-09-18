d40f7542ebd8a81e97049f5583d647b8534a5a10
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-18 20:15:33 +0100

    [ Servicio 'Fechas' ] » Nuevo método para solicitar todos los datos de una sentencia. Se ha eliminado el método para buscar fechas por su etiqueta en base a una secuencia. Código innecesario eliminado

--- src/app/services/fechas.service.ts
+++ src/app/services/fechas.service.ts
@@ -1,84 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-// Interfaces
-import { Fecha } from '../interfaces/fecha';
-
 @Injectable({
   providedIn: 'root'
 })
 export class FechasService {
 
-  fechas: any;
+  constructor( private http: HttpClient ) {}
 
-  constructor( private http: HttpClient ) { }
-
-  getFechas() {
+  getFechas() { // Función que solicita un JSON mediante HTTP (método GET) con el ID, la etiqueta, el día y la imagen de toda las fechas 
     return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarFechas');
-    // return this.http.get('http://localhost:55852/sentencias.asmx/MostrarFechas');
   }
 
-  getFecha(id: number) {
+  getFecha(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todos los datos de la fecha cuyo ID se envía como parámetro
     return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacion?id=' + id);
-    // return this.http.get('http://localhost:55852/MostrarInformacion?id=' + id);
   }
 
-  getSentencias(id: number) {
+  getSentencias(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todas las sentencias y sus autores de la fecha cuyo ID se envía como parámetro
     return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarSentencias?id=' + id);
-    // return this.http.get('http://localhost:55852/MostrarSentencias?id=' + id);
   }
 
-  getResultadosBusqueda (secuencia: string) {
-    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarResultadosBusqueda?secuencia=' + secuencia);
-    // return this.http.get('http://localhost:55852/sentencias.asmx/MostrarResultadosBusqueda?secuencia=' + secuencia);
-  }
-
-  /*
-
-    · Función que devuelve un vector de cuatro categorías seleccionadas aleatoriamente
-    · Ninguna de dichas categorías puede ser la que tenga el identificador pasado por parámetro
-    · ¿Por qué? Porque esta función se utiliza cuando hemos abierto una categoría y deseamos mostrar otras categorías
-    · No tiene sentido tener una categoría abierta y abajo mostrar un acceso directo a ella misma: es redundante
-
-  */
-  getFechasAleatorias(id: number, fechas: any[]) {
-
-    let posicion: number;
-    let resultados: any[] = new Array(3);
-
-    for (let i = 0; i < 3; i++) {
-
-      // Número aleatorio entre 0 y el tamaño del vector fechas - 1
-      do {
-        posicion = Math.floor(Math.random() * fechas.length);
-      } while (resultados.includes(fechas[posicion]) || posicion === id);
-
-      resultados[i] = fechas[posicion];
-    }
-
-    return resultados;
-  }
-
-  /*
-
-    · Función que devuelve un vector de fechas cuyo título albergue la secuencia pasada por parámetro
-
-  */
-  buscarFechas( secuencia: string ) {
-
-    let resultados: Fecha[] = [];
-    secuencia = secuencia.toLowerCase();
-
-    for (let fecha of this.fechas) {
-
-      let titulo: string = fecha.titulo;
-      titulo = titulo.toLowerCase();
-
-      if ( titulo.toString().indexOf(secuencia) >= 0 ) {
-        resultados.push(fecha);
-      }
-    }
-
-    return resultados;
+  getSentencia(id: number) {
+    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarDatosSentencia?id=' + id);
   }
 }
