af6245e19fae4b860dae6762339940e2b701b84b
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-20 13:13:01 +0100

    [ Servicio 'Fechas' ] » URLs actualizadas a HTTPS

--- src/app/services/fechas.service.ts
+++ src/app/services/fechas.service.ts
@@ -9,18 +9,18 @@ export class FechasService {
   constructor( private http: HttpClient ) {}
 
   getFechas() { // Función que solicita un JSON mediante HTTP (método GET) con el ID, la etiqueta, el día y la imagen de toda las fechas 
-    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarFechas');
+    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarFechas');
   }
 
   getDatosFecha(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todos los datos de la fecha cuyo ID se envía como parámetro
-    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionFecha?id=' + id);
+    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionFecha?id=' + id);
   }
 
   getSentencias(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todas las sentencias y sus autores de la fecha cuyo ID se envía como parámetro
-    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarSentencias?id=' + id);
+    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarSentencias?id=' + id);
   }
 
   getDatosSentencia(id: number) {
-    return this.http.get('http://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionSentencia?id=' + id);
+    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionSentencia?id=' + id);
   }
 }
