f0fa0317eb7333c2bca111b2ad388958f220d10d
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-21 19:24:31 +0100

    [ Servicio 'Fechas' ] » Cambio de notación en las URLs con parámetros. Se especifica el tipo devuelto

--- src/app/services/fechas.service.ts
+++ src/app/services/fechas.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// Interfaces
+import { Fecha     } from '../interfaces/fecha';
+import { Modal     } from '../interfaces/modal';
+import { Sentencia } from '../interfaces/sentencia';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,18 +14,18 @@ export class FechasService {
   constructor( private http: HttpClient ) {}
 
   getFechas() { // Función que solicita un JSON mediante HTTP (método GET) con el ID, la etiqueta, el día y la imagen de toda las fechas 
-    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarFechas');
+    return this.http.get<Fecha[]>('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarFechas');
   }
 
-  getDatosFecha(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todos los datos de la fecha cuyo ID se envía como parámetro
-    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionFecha?id=' + id);
+  getDatosFecha( id: number ) { // Función que solicita un JSON mediante HTTP (método GET) con todos los datos de la fecha cuyo ID se envía como parámetro
+    return this.http.get<Fecha>(`https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionFecha?id=${ id }`);
   }
 
-  getSentencias(id: number) { // Función que solicita un JSON mediante HTTP (método GET) con todas las sentencias y sus autores de la fecha cuyo ID se envía como parámetro
-    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarSentencias?id=' + id);
+  getSentencias( id: number ) { // Función que solicita un JSON mediante HTTP (método GET) con todas las sentencias y sus autores de la fecha cuyo ID se envía como parámetro
+    return this.http.get<Sentencia[]>(`https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarSentencias?id=${ id }`);
   }
 
-  getDatosSentencia(id: number) {
-    return this.http.get('https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionSentencia?id=' + id);
+  getDatosSentencia( id: number ) {
+    return this.http.get<Modal>(`https://appstip.iatext.ulpgc.es/ServicioSententiApp/sentencias.asmx/MostrarInformacionSentencia?id=${ id }`);
   }
 }
