237ee1f45c3d090257c9b87f514a0db343ff426d
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-24 15:41:53 +0100

    [ Componente 'Bottom Bar' ] » El color cambia en función de la página actual

--- src/app/components/bottombar/bottombar.component.ts
+++ src/app/components/bottombar/bottombar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bottombar',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./bottombar.component.scss'],
 })
 export class BottombarComponent {
+
+  @Input() color: string = 'dorado';
 
   constructor() { }
 
