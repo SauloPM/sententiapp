071548d270465a4bd4b2ca95cf314bc721886c05
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-11-19 10:09:00 +0000

    [ Componente 'menu' ] » El menú se cierra antes de cambiar de página

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, ViewChildren } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -14,56 +15,98 @@ export class MenuComponent implements OnInit {
 
   estado: string = 'cerrado';
 
-  constructor() { }
+  // ─────────────── //
+  //     MÉTODOS     //
+  // ─────────────── //
+
+  constructor( private router: Router ) { }
+
+  items = [
+    {
+      texto: 'Inicio',
+      enlace: 'home',
+      icono: 'home'
+    },
+    {
+      texto: 'Mis sentencias',
+      enlace: 'favoritos',
+      icono: 'star'
+    }
+  ];
 
   ngOnInit() {}
 
-  abrirMenu() {
+  mostrarMenu() {
 
     let menu = this.menu.nativeElement;
 
     const ABIERTO = menu.attributes['data-estado'].nodeValue === 'abierto' ? true : false;
     
-    if ( ABIERTO ) {
+    if ( ABIERTO )
+      this.cerrarMenu();
+    else
+      this.abrirMenu();
 
-      this.texto.first.nativeElement.style.opacity = '0';
-
-      setTimeout(() => {
-        this.texto.last.nativeElement.style.opacity = '0';
-      }, 150);
-
-      setTimeout(() => {
-        this.menu.nativeElement.attributes['data-estado'].nodeValue = 'cerrado';
-      }, 400);
-      
-      setTimeout(() => {
-        menu.style.backgroundColor = 'transparent';
-        this.botonMenu.nativeElement.className = 'boton';
-      }, 650);
-
-      setTimeout(() => {
-        menu.style.zIndex = '-1';
-      }, 900);
-
-    } else {
-
-      menu.style.zIndex = '50';
-      menu.style.backgroundColor = 'rgba(255,255,255,.75)';
-
-      this.botonMenu.nativeElement.className = 'boton abierto';
-
-      setTimeout(() => {
-        this.menu.nativeElement.attributes['data-estado'].nodeValue = 'abierto';
-      }, 250);
-
-      setTimeout(() => {
-        this.texto.first.nativeElement.style.opacity = '1';
-      }, 500);
-
-      setTimeout(() => {
-        this.texto.last.nativeElement.style.opacity = '1';
-      }, 650);
-
-    }
   }
+
+  navegar( enlace: string ) {
+    
+    this.cerrarMenu();
+
+    setTimeout( () => {
+      this.router.navigate( [ `/${ enlace }` ] );
+    }, 1000);
+
+  }
+
+  // ──────────────── //
+  //     AUXILIAR     //
+  // ──────────────── //
+
+  abrirMenu() {
+
+    let menu = this.menu.nativeElement;
+
+    menu.style.zIndex = '50';
+    menu.style.backgroundColor = 'rgba(255,255,255,.75)';
+
+    this.botonMenu.nativeElement.className = 'boton abierto';
+
+    setTimeout(() => {
+      this.menu.nativeElement.attributes['data-estado'].nodeValue = 'abierto';
+    }, 250);
+
+    setTimeout(() => {
+      this.texto.first.nativeElement.style.opacity = '1';
+    }, 500);
+
+    setTimeout(() => {
+      this.texto.last.nativeElement.style.opacity = '1';
+    }, 650);
+  }
+
+  cerrarMenu() {
+
+    let menu = this.menu.nativeElement;
+
+    this.texto.first.nativeElement.style.opacity = '0';
+
+    setTimeout(() => {
+      this.texto.last.nativeElement.style.opacity = '0';
+    }, 150);
+
+    setTimeout(() => {
+      this.menu.nativeElement.attributes['data-estado'].nodeValue = 'cerrado';
+    }, 400);
+    
+    setTimeout(() => {
+      menu.style.backgroundColor = 'transparent';
+      this.botonMenu.nativeElement.className = 'boton';
+    }, 650);
+
+    setTimeout(() => {
+      menu.style.zIndex = '-1';
+    }, 900);
+  }
+
 }
