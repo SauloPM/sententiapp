f2312225b13bb7be86dfeece40d730a4dc705258
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-11-20 11:23:53 +0000

    [ Página 'warning' ] » Estilos actualizados

--- src/app/pages/warning/warning.page.ts
+++ src/app/pages/warning/warning.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
 })
 export class WarningPage {
 
-  mensaje = 'SententiApp selecciona sentencias de autores clásicos traducidas al español e inglés sobre temas de actualidad como ejemplos de la pervivencia y de la transformación del pensamiento clásico en la sociedad occidental. Léelas, da tu opinión y utilízalas para tus argumentos. SententiApp no se hace responsable de las opiniones de los autores antiguos.';
+  mensaje = 'SententiApp selecciona sentencias de autores clásicos traducidas al español e inglés sobre temas de actualidad como ejemplos de la pervivencia y de la transformación del pensamiento clásico en la sociedad occidental. Léelas, da tu opinión y utilízalas para tus argumentos.';
+
+  aclaracion = 'SententiApp no se hace responsable de las opiniones de los autores antiguos';
 
   constructor() { }
   
