b6d6111f8a49e8c1cf53fbdcc1f96813c98e1a32
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-02-26 22:06:22 +0000

    [ Firebase ] » Creada variable con los parámetros de configuración de Firebase

---
+++ src/environments/firebase-configuration.ts
@@ -0,0 +1,10 @@
+export const firebaseConfiguration = {
+  apiKey: "AIzaSyAriN1CWBrUtmhvb8u51qnagBAo8C8s_W0",
+  authDomain: "sententiapp-889ef.firebaseapp.com",
+  databaseURL: "https://sententiapp-889ef.firebaseio.com",
+  projectId: "sententiapp-889ef",
+  storageBucket: "sententiapp-889ef.appspot.com",
+  messagingSenderId: "557696056339",
+  appId: "1:557696056339:web:c8a791f258f3831dd6d3e6",
+  measurementId: "G-202SDYDMEK"
+};
