a8226f919950d8ff54ff35d60dda017affcde620
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-03-07 18:51:36 +0000

    [ Página 'favoritos' ] » Código refactorizado (se resalta la reacción seleccionada del slider de reacciones en un método aparte en lugar de dentro del hook OnInit)

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -20,23 +20,23 @@ declare var $: any;
 })
 export class FavoritosPage implements OnInit {
   
+  vacio = true;
   deviceID: string;
   fechas: Fecha[];
-  vacio = true;
-  favoritos : any[];
-
+  
   reacciones = [
-    { value: 'Todos', key: 'todos' },
-    { value: 'Me gusta', key: 'me-gusta' },
-    { value: 'Me encanta', key: 'me-encanta' },
+    { value: 'Todos'      , key: 'todos'       },
+    { value: 'Me gusta'   , key: 'me-gusta'    },
+    { value: 'Me encanta' , key: 'me-encanta'  },
     { value: 'Me divierte', key: 'me-divierte' },
     { value: 'No me gusta', key: 'no-me-gusta' },
   ];
-
+  
   reaccionSeleccionada = 'todos';
   
   sentencias        : Sentencia[];
   todasLasSentencias: Sentencia[];
+  sentenciasFirebase: any[];
 
   configuracion = {
     spaceBetween: 0,
@@ -55,41 +55,45 @@ export class FavoritosPage implements OnInit {
   
   async ngOnInit() {
 
+    // Obtenemos el ID de este dispositivo
     await this.uniqueDeviceID.get()
-      .then (( data : any ) => this.deviceID = data       )
+      .then (( id   : any ) => this.deviceID = id   )
       .catch(( error: any ) => this.deviceID = '74a1eb27' );
 
     // Obtenemos todas las fechas (para enviarlas al componente 'Otras fechas')
     this.servicioFechas.getFechas().subscribe( data => {
       this.fechas = data;
 
-      // Obtenemos todas las sentencias
-      this.servicioFechas.getSentencias().subscribe( ( data: Sentencia[] ) => {
+      // Obtenemos las sentencias de todas las fechas
+      this.servicioFechas.getSentencias().subscribe(( data: Sentencia[] ) => {
         this.todasLasSentencias = data;
 
-        // Obtenemos las sentencias favoritas del usuario almacenadas en Firebase
+        // Obtenemos las sentencias almacenadas en Firebase de este dispositivo
         this.servicioFavoritos.getSentencias().subscribe(( data: any[] ) => {
-          this.favoritos = data.filter( item => item.id_dispositivo === this.deviceID );
+          this.sentenciasFirebase = data.filter( item => item.id_dispositivo === this.deviceID );
           
+          // De todas las sentencias de todas las fechas, solo nos interesan aquellas para las que haya reaccionado desde este dispositivo
           this.todasLasSentencias = this.filtrarSentenciasPorReaccion();
           this.sentencias = this.todasLasSentencias;
           this.vacio = this.sentencias === undefined ? true : this.sentencias.length === 0 ? true : false;
         });
       });
     });
-
-    // Seleccionar categoría del filtro
-    $( document ).on( 'click', '.filtro .item', function() {
-      $( '.filtro .item.seleccionado' ).removeClass( 'seleccionado' );
-      $( this ).addClass( 'seleccionado' );
-    });
   }
 
+  // Seleccionar una reacción del slider de reacciones
   cambiarReaccion( reaccion: string ) {
+
+    // Resaltamos la reacción seleccionada en el slider de reacciones
+    $( '.filtro .item.seleccionado'  ).removeClass( 'seleccionado' );
+    $( `.filtro .item.${ reaccion }` ).addClass   ( 'seleccionado' );
+
+    // Actualizamos el listado de sentencias
     this.reaccionSeleccionada = reaccion;
     this.actualizarFavoritos();
   }
 
+  // Cambiar la reacción de una sentencia
   actualizarFavoritos() {
     this.sentencias = this.filtrarSentenciasPorReaccion( this.reaccionSeleccionada );
     this.vacio = this.sentencias === undefined ? true : this.sentencias.length === 0 ? true : false;
@@ -103,13 +107,18 @@ export class FavoritosPage implements OnInit {
 
     let resultado: Sentencia[] = [];
 
-    for ( let i = 0; i < this.favoritos.length; i++) {
-      let sentenciaEncontrada = this.todasLasSentencias.find( item => item.id == this.favoritos[i].id_sentencia );
+    for ( let i = 0; i < this.sentenciasFirebase.length; i++) {
+
+      // De todas las sentencias solo nos interesan las favoritas del usuario 
+      let sentenciaEncontrada = this.todasLasSentencias.find( item => item.id == this.sentenciasFirebase[i].id_sentencia );
+
+      // Almacenamos la sentencia encontrada en una variable auxiliar
       if ( sentenciaEncontrada != undefined ) {
         resultado.push( sentenciaEncontrada );
       }
     }
 
+    // De todas las sentencias favoritas del usuario, solo nos interesan las de una reacción determinada
     return reaccion == 'todos' ? resultado : resultado.filter( item => item.reaccion == reaccion );
   }
 }
