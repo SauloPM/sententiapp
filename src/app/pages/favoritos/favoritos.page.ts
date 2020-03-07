e8c3816950f8105c8319a76d0480c73d63ce0e8f
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-03-07 17:51:30 +0000

    [ Componente 'favoritos' ] » Código refactorizado. Código adaptado para trabajar con Firebase. Código para trabajar con el almacenamiento local del dispositivo eliminado

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -8,6 +8,9 @@ import { Fecha } from 'src/app/interfaces/fecha';
 import { FechasService    } from './../../services/fechas.service';
 import { FavoritosService } from './../../services/favoritos.service';
 
+// Obtener ID único del dispositivo
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
+
 // jQuery
 declare var $: any;
 
@@ -16,12 +19,24 @@ declare var $: any;
   templateUrl: './favoritos.page.html'
 })
 export class FavoritosPage implements OnInit {
-
+  
+  deviceID: string;
   fechas: Fecha[];
-  vacio: boolean = true;
-  favoritos : Sentencia[];
-  reacciones: string[] = [ 'Todos', 'Me gusta', 'Me encanta', 'Me divierte', 'No me gusta' ];
-  reaccionSeleccionada: string = 'Todos';
+  vacio = true;
+  favoritos : any[];
+
+  reacciones = [
+    { value: 'Todos', key: 'todos' },
+    { value: 'Me gusta', key: 'me-gusta' },
+    { value: 'Me encanta', key: 'me-encanta' },
+    { value: 'Me divierte', key: 'me-divierte' },
+    { value: 'No me gusta', key: 'no-me-gusta' },
+  ];
+
+  reaccionSeleccionada = 'todos';
+  
+  sentencias        : Sentencia[];
+  todasLasSentencias: Sentencia[];
 
   configuracion = {
     spaceBetween: 0,
@@ -36,36 +51,65 @@ export class FavoritosPage implements OnInit {
   //     MÉTODOS     //
   // ─────────────── //
 
-  constructor( private servicioFechas: FechasService, public servicioFavoritos: FavoritosService ) {}
+  constructor( private servicioFechas: FechasService, public servicioFavoritos: FavoritosService, private uniqueDeviceID: UniqueDeviceID ) {}
   
-  ngOnInit() {
+  async ngOnInit() {
 
-    // Obtenemos todas las fechas
+    await this.uniqueDeviceID.get()
+      .then (( data : any ) => this.deviceID = data       )
+      .catch(( error: any ) => this.deviceID = '74a1eb27' );
+
+    // Obtenemos todas las fechas (para enviarlas al componente 'Otras fechas')
     this.servicioFechas.getFechas().subscribe( data => {
       this.fechas = data;
-    });
 
-    setTimeout( () => {
-      this.favoritos = this.servicioFavoritos.favoritos;
-      this.vacio = this.favoritos === undefined ? true : this.favoritos.length === 0 ? true : false;
-    }, 500);
+      // Obtenemos todas las sentencias
+      this.servicioFechas.getSentencias().subscribe( ( data: Sentencia[] ) => {
+        this.todasLasSentencias = data;
+
+        // Obtenemos las sentencias favoritas del usuario almacenadas en Firebase
+        this.servicioFavoritos.getSentencias().subscribe(( data: any[] ) => {
+          this.favoritos = data.filter( item => item.id_dispositivo === this.deviceID );
+          
+          this.todasLasSentencias = this.filtrarSentenciasPorReaccion();
+          this.sentencias = this.todasLasSentencias;
+          this.vacio = this.sentencias === undefined ? true : this.sentencias.length === 0 ? true : false;
+        });
+      });
+    });
 
     // Seleccionar categoría del filtro
-    $(document).on('click', '.filtro .item', function() {
-      $('.filtro .item.seleccionado').removeClass('seleccionado');
-      $(this).addClass('seleccionado');
+    $( document ).on( 'click', '.filtro .item', function() {
+      $( '.filtro .item.seleccionado' ).removeClass( 'seleccionado' );
+      $( this ).addClass( 'seleccionado' );
     });
-
   }
 
   cambiarReaccion( reaccion: string ) {
     this.reaccionSeleccionada = reaccion;
-    this.favoritos = reaccion === 'Todos' ? this.servicioFavoritos.favoritos : this.servicioFavoritos.favoritos.filter( item => item.reaccion === reaccion );
-    this.vacio = this.favoritos === undefined ? true : this.favoritos.length === 0 ? true : false;
+    this.actualizarFavoritos();
   }
 
   actualizarFavoritos() {
-    this.favoritos = this.reaccionSeleccionada === 'Todos' ? this.servicioFavoritos.favoritos : this.servicioFavoritos.favoritos.filter( item => item.reaccion === this.reaccionSeleccionada )
-    this.vacio = this.favoritos === undefined ? true : this.favoritos.length === 0 ? true : false;
+    this.sentencias = this.filtrarSentenciasPorReaccion( this.reaccionSeleccionada );
+    this.vacio = this.sentencias === undefined ? true : this.sentencias.length === 0 ? true : false;
+  }
+
+  // ──────────────── //
+  //     AUXILIAR     //
+  // ──────────────── //
+
+  filtrarSentenciasPorReaccion( reaccion: string = 'todos' ) {
+
+    let resultado: Sentencia[] = [];
+
+    for ( let i = 0; i < this.favoritos.length; i++) {
+      let sentenciaEncontrada = this.todasLasSentencias.find( item => item.id == this.favoritos[i].id_sentencia );
+      if ( sentenciaEncontrada != undefined ) {
+        resultado.push( sentenciaEncontrada );
+      }
+    }
+
+    return reaccion == 'todos' ? resultado : resultado.filter( item => item.reaccion == reaccion );
   }
 }
