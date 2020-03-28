3ec21dcda6828e76c238bd52a1ad71e7afc52268
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-03-28 22:40:40 +0000

    [ Servicio 'usuarios' ] » Creación de servicio para gestionar el ID del dispositivo sin necesidad del plugin de Cordova 'UniqueDeviceId'

---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+// Firebase
+import { AngularFirestore } from '@angular/fire/firestore';
+
+// Almacenamiento local
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor( private angularFirestore: AngularFirestore, private storage: Storage ) {}
+
+  // ───────────────────── //
+  //     LOCAL STORAGE     //
+  // ───────────────────── //
+
+  async getUsuarioID() {
+
+    const usuarioID = await this.storage.get( 'usuarioID' );
+
+    return usuarioID ? usuarioID : '-1';
+
+  }
+
+  guardarUsuarioEnLocalStorage( usuarioID: string ) {
+
+    this.storage.set( 'usuarioID', usuarioID );
+
+  }
+
+  // ───────────────── //
+  //     FIRESTORE     //
+  // ───────────────── //
+
+  async crearUsuario() {
+
+    let fecha = new Date();
+
+    // Creamos un nuevo documento en la colección de usuarios de Firestore y luego almacenamos el ID del usuario en el local storage
+    await this.angularFirestore.collection( 'usuarios' ).add({ fecha: fecha.getFullYear() }).then( documento => this.guardarUsuarioEnLocalStorage( documento.id ));
+
+  }
+
+  actualizarUsuario( usuarioID: string, fecha: Date ) {
+    this.angularFirestore.collection( 'usuarios' ).doc( usuarioID ).update({
+      fecha: fecha.getFullYear()
+    });
+  }
+
+  eliminarUsuario( usuarioID: string ) {
+    this.angularFirestore.collection( 'usuarios' ).doc( usuarioID ).delete();
+  }
+}
