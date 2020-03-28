750714eb88b55ad5c750369d670b11ebb4c19f9b
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-03-28 22:46:49 +0000

    [ Componente 'sentencias' ] » Se gestiona el ID del dispositivo con el servicio en lugar de con el plugin de Cordova 'UniqueDeviceId'

--- src/app/components/sentencias/sentencias.component.ts
+++ src/app/components/sentencias/sentencias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 // Servicios
+import { UsuariosService  } from '../../services/usuarios.service';
 import { FavoritosService } from '../../services/favoritos.service';
 
 // Interfaces
@@ -8,9 +9,6 @@ import { Sentencia } from '../../interfaces/sentencia';
 
 // Compartir en RRSS
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
-
-// Obtener ID único del dispositivo
-import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
 
 // jQuery
 declare var $: any;
@@ -26,7 +24,7 @@ export class SentenciasComponent implements OnInit {
 
   @Output() dispararRefrescarListadoDeSentenciasFavoritas: EventEmitter<any>;
 
-  deviceID: string;
+  usuarioID: string;
 
   sentenciasFirebase: any;
 
@@ -43,16 +41,19 @@ export class SentenciasComponent implements OnInit {
   //     MÉTODOS     //
   // ─────────────── //
 
-  constructor( private socialSharing: SocialSharing, private servicioFavoritos: FavoritosService, private uniqueDeviceID: UniqueDeviceID ) {
+  constructor( private socialSharing: SocialSharing, private servicioFavoritos: FavoritosService, private usuariosService: UsuariosService ) {
     this.dispararRefrescarListadoDeSentenciasFavoritas = new EventEmitter();
   }
 
   async ngOnInit() {
 
-    await this.uniqueDeviceID.get()
-      .then (( id   : any ) => this.deviceID = id   )
-      .catch(( error: any ) => this.deviceID = '-1' );
+    // Almacenamos en una variable el ID del usuario
+    await this.usuariosService.getUsuarioID().then(
+      data  => this.usuarioID = data,
+      error => console.log( error )
+    );
 
+    // Almacenamos en una variable las sentencias a las que el usuario reaccionó
     this.servicioFavoritos.getSentencias().subscribe(( data: any ) => {
 
       this.sentenciasFirebase = data;
@@ -64,39 +65,42 @@ export class SentenciasComponent implements OnInit {
     });
   }
 
-  guardarFavorito( sentencia: Sentencia, reaccion: string ) {
+  async guardarFavorito( sentencia: Sentencia, reaccion: string ) {
 
-    if ( this.deviceID === '-1' ) {
-      this.notificarUsuario();
+    if ( this.usuarioID === '-1' ) {
+      await this.usuariosService.crearUsuario();
+      await this.usuariosService.getUsuarioID().then(
+        data  => this.usuarioID = data,
+        error => console.log( 'Se ha producido un error' )
+      );
+    }
+
+    if ( this.usuarioID === '-1' ) {
       return;
     }
 
     sentencia.reaccion = reaccion;
     
     // ¿Habíamos reaccionado previamente a la sentencia a la que acabamos de reaccionar?
-    const EXISTE = this.sentenciasFirebase.find( item => item.id_dispositivo === this.deviceID && item.id_sentencia === sentencia.id );
+    const EXISTE = this.sentenciasFirebase.find( item => item.id_dispositivo === this.usuarioID && item.id_sentencia === sentencia.id );
 
     // En caso negativo, se crea en Firebase un nuevo registro para alojar la nueva sentencia favorita
     if ( !EXISTE ) {
-      this.servicioFavoritos.crear( sentencia.id, this.deviceID, reaccion );
+      this.servicioFavoritos.crear( sentencia.id, this.usuarioID, reaccion );
     }
 
     // En caso positivo, se actualiza en Firebase la reacción del registro correspondiente
     else {
       sentencia.reaccion = reaccion;
-      this.servicioFavoritos.actualizar( sentencia.id, this.deviceID, reaccion );
+      this.servicioFavoritos.actualizar( sentencia.id, this.usuarioID, reaccion );
     }
   }
 
   eliminarFavorito ( sentencia: Sentencia, reaccion: string ) {
 
-    if ( this.deviceID === '-1' ) {
-      this.notificarUsuario();
-      return;
-    }
-
     sentencia.reaccion = '';
-    this.servicioFavoritos.eliminar( this.deviceID, sentencia.id );
+    this.servicioFavoritos.eliminar( this.usuarioID, sentencia.id );
+
   }
 
   compartir( sentencia: Sentencia ) {
@@ -105,7 +109,7 @@ export class SentenciasComponent implements OnInit {
     let extractosActivos = document.getElementsByClassName( 'extracto swiper-slide-active' );
 
     // Detectamos qué traducción del extraco se desea compartir
-    for (let i = 0; i < extractosActivos.length ; i++) {
+    for (let i = 0; i < extractosActivos.length; i++) {
       
       if ( extractosActivos[i].textContent.trim() === sentencia.extractolatino.trim() ) {
         extracto = sentencia.extractolatino;
@@ -133,13 +137,15 @@ export class SentenciasComponent implements OnInit {
 
   resaltarSentencias() {
 
+    console.log( this.usuarioID );
+
     let busqueda: any;
-    let misFavoritos = this.sentenciasFirebase.filter( item => item.id_dispositivo === this.deviceID );
+    let misFavoritos = this.sentenciasFirebase.filter( item => item.id_dispositivo === this.usuarioID );
 
     this.sentencias.forEach(( sentencia ) => {
 
       // Resaltamos la reacción
-      busqueda = misFavoritos.find( item => item.id_dispositivo === this.deviceID && item.id_sentencia === sentencia.id );
+      busqueda = misFavoritos.find( item => item.id_dispositivo === this.usuarioID && item.id_sentencia === sentencia.id );
 
       if ( busqueda != undefined ) sentencia.reaccion = busqueda.reaccion;
 
@@ -154,14 +160,5 @@ export class SentenciasComponent implements OnInit {
       if ( recuentoMeDivierte != undefined ) sentencia.recuentoMeDivierte = recuentoMeDivierte.length = recuentoMeDivierte.length;
       if ( recuentoNoMeGusta  != undefined ) sentencia.recuentoNoMeGusta  = recuentoNoMeGusta .length = recuentoNoMeGusta .length;
     });
-  }
-
-  notificarUsuario() {
-
-    $( '.notificacion' ).css( 'opacity', '1' );
-
-    setTimeout(() => {
-      $( '.notificacion' ).css( 'opacity', '' );
-    }, 2500);
   }
 }
