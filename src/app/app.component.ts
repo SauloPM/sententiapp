d0811b67ae196b5b88ffca00dc32bca1c05638c3
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-08-27 15:24:05 +0100

    [ Componente 'app' ] » Código innecesario eliminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,28 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Router } from '@angular/router';
-
-import { Platform } from '@ionic/angular';
-
+import { Router       } from '@angular/router';
+import { Platform     } from '@ionic/angular';
 import { StatusBar    } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
+// Notificaciones push
 import { PushService } from './services/push.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent {
   
-  backButtonSubscription;
-
   constructor( public router: Router, private platform: Platform, private statusBar: StatusBar, private pushService: PushService, private splashScreen: SplashScreen ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    
     this.platform.ready().then(() => {
 
-      // Esto se hace porque el topbar se pone en negro
-      // this.statusBar.styleDefault();
+      // Evitamos que el topbar aparezca en negro
       if (this.platform.is('android')) {
         this.statusBar.styleBlackOpaque();
       }
@@ -34,15 +31,5 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
       this.pushService.configuracionInicial();
     });
-  }
-
-  ngAfterViewInit() {
-    this.backButtonSubscription = this.platform.backButton.subscribe(() => {
-      navigator['app'].exitApp();
-    });
-  }
-
-  ngOnDestroy() {
-    this.backButtonSubscription.unsubscribe();
   }
 }
